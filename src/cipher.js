444b5fc2128ef2203dcca296ce84456b25b0a402
NellyGonzalezB <nellygonzalezb.92@gmail.com>
2021-02-23 11:56:48 -0300

    anadi espacios

--- src/cipher.js
+++ src/cipher.js
@@ -1,45 +1,56 @@
- /*FÓRMULA E INDICACIONES PARA CIFRAR*/
- const cipher ={
-  encode :(offset, string)=>{
-  let resultado = "";
-  let mensaje = "";
-  for (let i=0; i<string.length;i++){
-      if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
-      mensaje = ((string.charCodeAt(i)-65+parseInt(offset))%26)+65;
-      mensaje = String.fromCharCode(mensaje);
+//FÓRMULA E INDICACIONES PARA CIFRAR
+const cipher = {
+  encode: (offset, string,) => {
+    let resultado = "";
+    let mensaje = "";
+    for (let i = 0; i < string.length; i++) {
+      //Fórmula letras mayúsculas
+      if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+        mensaje = ((string.charCodeAt(i) - 65 + parseInt(offset)) % 26) + 65;
+      } 
       
+      //Fórmula letras minúsculas
+      else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+        mensaje = ((string.charCodeAt(i) - 97 + parseInt(offset)) % 26) + 97;
+      }  
+      
+      //Espacio en el Número 32 del Código ASCII
+      else if (string.charCodeAt(i)===32){
+        mensaje=32;
       }
-      else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
-      mensaje = ((string.charCodeAt(i)-97+ parseInt(offset))%26)+97;
-      mensaje = String.fromCharCode(mensaje);
+
+       resultado += String.fromCharCode(mensaje);
       }
-      resultado += mensaje;
-      }
+      
+    
       return resultado;
-      },
+  },
+
+  //FÓRMULA E INDICACIONES PARA DESCIFRAR
+  decode: (offset, string) => {
+    let resultado = "";
+    let mensaje = "";
+    for (let i = 0; i < string.length; i++) {
+      //Fórmula letras mayúsculas
+      if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+        mensaje = ((string.charCodeAt(i) - 90 -  parseInt(offset)) % 26) + 90;
+      } 
       
-/*FÓRMULA E INDICACIONES PARA DESCIFRAR*/
-  decode :(offset, string)=>{
-  let resultado = "";
-  let mensaje = "";
-  for (let i=0; i<string.length;i++){
-      if (string.charCodeAt(i)>=65 && string.charCodeAt(i)<=90){
-      mensaje = ((string.charCodeAt(i)-65-parseInt(offset))%26)+65;
-      mensaje = String.fromCharCode(mensaje);
+      //Fórmula letras minúsculas
+      else if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+        mensaje = ((string.charCodeAt(i) - 122 - parseInt(offset)) % 26) + 122;
+      }
       
+      //Espacio en el Número 32 del Código ASCII
+      else if (string.charCodeAt(i)===32){
+        mensaje=32;
       }
-      else if (string.charCodeAt(i)>=97 && string.charCodeAt(i)<=122){
-      mensaje = ((string.charCodeAt(i)-97-parseInt(offset))%26)+97;
-      mensaje = String.fromCharCode(mensaje);
+    
+      resultado += String.fromCharCode(mensaje);
       }
-      resultado += mensaje;
-      }
+
       return resultado;
-
-   }
-}
-
-
-
+  
+}}
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 import cipher from './cipher.js';
-/*PÁGINAS PRINCIPALES CON LA FUNCIÓN DE OCULTAR Y MOSTRAR*/
+//PÁGINAS PRINCIPALES CON LA FUNCIÓN DE OCULTAR Y MOSTRAR
 document.getElementById("pag1").style.display="block";
 document.getElementById("pag2").style.display="none";
 
-/*INDICACIÓN DE QUE AL MOMENTO DE CLICKEAR COMENZAR LA PÁGINA UNO DESAPAREZCA*/
+//INDICACIÓN DE QUE AL MOMENTO DE CLICKEAR COMENZAR LA PÁGINA UNO DESAPAREZCA
 let comenzar=document.getElementById("comenzar");
 comenzar.addEventListener("click", pagina2);
 function pagina2(){
   document.getElementById("pag1").style.display="none";
   document.getElementById("pag2").style.display="block";
 }
-/*INDICAIÓN PARA VOLVER A LA PÁGINA 1 PRESIONANDO EL LOGO*/
+//INDICAIÓN PARA VOLVER A LA PÁGINA 1 PRESIONANDO EL LOGO
 let PaginaInicio=document.getElementById("logo");
 PaginaInicio.addEventListener("click", volver);
 function volver(){
   document.getElementById("pag1").style.display="block";
   document.getElementById("pag2").style.display="none";
 }
-/*LLAMAR FUNCIONES CIFRAR, DESCIFRAR Y RESULTADO, Y UNIENDOLO CON CIPHER.JS*/
+//LLAMAR FUNCIONES CIFRAR, DESCIFRAR Y RESULTADO, Y UNIENDOLO CON CIPHER.JS
 document.getElementById("cifrar").addEventListener("click", () => {
     let string = document.getElementById("mensaje").value;
     let offset = document.getElementById("desplazar").value;
